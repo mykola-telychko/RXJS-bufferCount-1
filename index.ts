fa818b9becce619cbeb779664f4991fc514cc7ff
mykola-telychko <75166107+mykola-telychko@users.noreply.github.com>
2023-08-26 23:35:30 +0300

    fin

--- index.ts
+++ index.ts
@@ -1,12 +1,15 @@
 import { interval } from 'rxjs';
 import { bufferCount } from 'rxjs/operators';
 
+// https://www.learnrxjs.io/learn-rxjs/operators/transformation/buffercount
+
+// Example 1: Collect buffer and emit after specified number of values
 //Create an observable that emits a value every second
-const source = interval(1000);
+const srcInterval$ = interval(1000);
 //After three values are emitted, pass on as an array of buffered values
-const bufferThree = source.pipe(bufferCount(3));
+const bufferThree = srcInterval$.pipe(bufferCount(3));
 //Print values to console
 //ex. output [0,1,2]...[3,4,5]
-const subscribe = bufferThree.subscribe(val =>
+const subscribe = bufferThree.subscribe((val) =>
   console.log('Buffered Values:', val)
 );
